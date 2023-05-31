4c16978f43a1c943088ed983fe144b3d265a23ae
Eric V.K <123314997+ericvk7@users.noreply.github.com>
2023-05-31 22:15:56 +0700

    add db

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const PORT = 8001;
+const { db, query } = require("./database");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log("Server is running on port: " + PORT);
+});
