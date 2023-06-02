c571e6876694846f9168aa83b6d4485222c0752b
annisafrl <annisafirlia1@gmail.com>
2023-06-02 17:05:30 +0700

    update login be

--- projects/server/controllers/authController.js
+++ projects/server/controllers/authController.js
@@ -4,9 +4,12 @@ const jwt = require("jsonwebtoken");
 
 module.exports = {
   register: async (req, res) => {
-    const { username, email, phone, store_name, password } = req.body;
+    const { email, password, phoneNumber } = req.body;
 
-    let getEmailQuery = `SELECT * FROM users WHERE email=${db.escape(email)}`;
+    let getEmailQuery = `SELECT * FROM users WHERE user_email=${db.escape(
+      email
+    )}`;
+
     let isEmailExist = await query(getEmailQuery);
     if (isEmailExist.length > 0) {
       return res.status(200).send({ message: "Email has been used" });
@@ -16,11 +19,13 @@ module.exports = {
     const hashPassword = await bcrypt.hash(password, salt);
 
     let addUserQuery = `INSERT INTO users VALUES (null, ${db.escape(
-      username
-    )}, ${db.escape(email)}, ${db.escape(phone)}, ${db.escape(
-      store_name
-    )}, ${db.escape(hashPassword)})`;
+      email
+    )}, ${db.escape(hashPassword)}, ${db.escape(
+      phoneNumber
+    )}, null, null, null, null, null, null)`;
     let addUserResult = await query(addUserQuery);
+
+    console.log(req.body);
 
     let payload = { id: addUserResult.insertId };
     const token = jwt.sign(payload, "six6", { expiresIn: "4h" });
@@ -32,38 +37,36 @@ module.exports = {
   login: async (req, res) => {
     try {
       const { email, password } = req.body;
-      const isEmailExist = await query(
-        `SELECT * FROM users WHERE email=${db.escape(email)}`
+      let isEmailExist = await query(
+        `SELECT * FROM users WHERE user_email=${db.escape(email)}`
       );
+      console.log(isEmailExist);
+
       if (isEmailExist.length == 0) {
         return res
           .status(200)
           .send({ message: "Email or Password is Invalid", success: false });
       }
-
-      const isValid = await bcrypt.compare(password, isEmailExist[0].password);
-
+      const isValid = await bcrypt.compare(
+        password,
+        isEmailExist[0].user_password
+      );
       if (!isValid) {
         return res
           .status(200)
           .send({ message: "Email or Password is incorrect", success: false });
       }
-
       let payload = {
         id: isEmailExist[0].id_users,
       };
-
       const token = jwt.sign(payload, "six6", { expiresIn: "1h" });
-
       return res.status(200).send({
         message: "Login Success",
         token,
         data: {
           id: isEmailExist[0].id_users,
-          username: isEmailExist[0].username,
-          email: isEmailExist[0].email,
-          phone: isEmailExist[0].phone,
-          storeName: isEmailExist[0].store_name,
+          email: isEmailExist[0].email_email,
+          phone: isEmailExist[0].user_phone_number,
         },
         success: true,
       });
@@ -73,45 +76,45 @@ module.exports = {
   },
 
   fetchAllUser: async (req, res) => {
-    try {
-      const users = await query(`SELECT * FROM users`);
-      return res.status(200).send(users);
-    } catch (error) {
-      res.status(error.status || 500).send(error);
-    }
+    // try {
+    //   const users = await query(`SELECT * FROM users`);
+    //   return res.status(200).send(users);
+    // } catch (error) {
+    //   res.status(error.status || 500).send(error);
+    // }
   },
   fetchUser: async (req, res) => {
-    try {
-      const idParams = parseInt(req.params.id);
-      if (req.user.id !== idParams) {
-        return res.status(400).send("Unauthorized attempt");
-      }
-      const users = await query(
-        `SELECT * FROM users WHERE id_users = ${db.escape(idParams)}`
-      );
-      return res.status(200).send(users);
-    } catch (error) {
-      res.status(error.status || 500).send(error);
-    }
+    // try {
+    //   const idParams = parseInt(req.params.id);
+    //   if (req.user.id !== idParams) {
+    //     return res.status(400).send("Unauthorized attempt");
+    //   }
+    //   const users = await query(
+    //     `SELECT * FROM users WHERE id_users = ${db.escape(idParams)}`
+    //   );
+    //   return res.status(200).send(users);
+    // } catch (error) {
+    //   res.status(error.status || 500).send(error);
+    // }
   },
 
   checkLogin: async (req, res) => {
-    try {
-      const users = await query(
-        `SELECT * FROM users WHERE id_users = ${db.escape(req.user.id)}`
-      );
-      console.log(users);
-      return res.status(200).send({
-        data: {
-          id: users[0].id_users,
-          username: users[0].username,
-          email: users[0].email,
-          phone: users[0].phone,
-          storeName: users[0].store_name,
-        },
-      });
-    } catch (error) {
-      res.status(error.status || 500).send(error);
-    }
+    // try {
+    //   const users = await query(
+    //     `SELECT * FROM users WHERE id_users = ${db.escape(req.user.id)}`
+    //   );
+    //   console.log(users);
+    //   return res.status(200).send({
+    //     data: {
+    //       id: users[0].id_users,
+    //       username: users[0].username,
+    //       email: users[0].email,
+    //       phone: users[0].phone,
+    //       storeName: users[0].store_name,
+    //     },
+    //   });
+    // } catch (error) {
+    //   res.status(error.status || 500).send(error);
+    // }
   },
 };
