be0f956b49a89e931d97b612de75e69923bbde7b
annisafrl <annisafirlia1@gmail.com>
2023-06-05 20:38:32 +0700

    update auth

--- projects/server/routes/authRoutes.js
+++ projects/server/routes/authRoutes.js
@@ -4,7 +4,7 @@ const { verifyToken } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.post("/", authController.register);
+router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/check-login", verifyToken, authController.checkLogin);
 router.get("/user", verifyToken, authController.fetchAllUser);
