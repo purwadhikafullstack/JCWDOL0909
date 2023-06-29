0224a57a23588bbe597e4ac975aa008b4d479665
Eric V.K <123314997+ericvk7@users.noreply.github.com>
2023-06-29 08:59:11 +0700

    Add UploadForm

--- projects/client/src/pages/Transaction/Transaction.jsx
+++ projects/client/src/pages/Transaction/Transaction.jsx
@@ -16,18 +16,18 @@ function Transaction() {
 
   return (
     <>
-      <div class="flex flex-col items-center  border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
-        <div class="mt-4 py-2 text-xs sm:mt-0 sm:ml-auto sm:text-base ">
-          <div class="relative">
-            <ul class="relative flex w-full items-center justify-between space-x-2 sm:space-x-4">
-              <li class="flex items-center space-x-3 text-left sm:space-x-4">
+      <div className="flex flex-col items-center  border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
+        <div className="mt-4 py-2 text-xs sm:mt-0 sm:ml-auto sm:text-base ">
+          <div className="relative">
+            <ul className="relative flex w-full items-center justify-between space-x-2 sm:space-x-4">
+              <li className="flex items-center space-x-3 text-left sm:space-x-4">
                 <a
-                  class="flex h-6 w-6 items-center justify-center rounded-full bg-emerald-200 text-xs font-semibold text-emerald-700"
+                  className="flex h-6 w-6 items-center justify-center rounded-full bg-emerald-200 text-xs font-semibold text-emerald-700"
                   href="#"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    class="h-4 w-4"
+                    className="h-4 w-4"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -40,11 +40,11 @@ function Transaction() {
                     />
                   </svg>
                 </a>
-                <span class="font-semibold text-gray-900">Shop</span>
+                <span className="font-semibold text-gray-900">Shop</span>
               </li>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-4 w-4 text-gray-400"
+                className="h-4 w-4 text-gray-400"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -56,18 +56,18 @@ function Transaction() {
                   d="M9 5l7 7-7 7"
                 />
               </svg>
-              <li class="flex items-center space-x-3 text-left sm:space-x-4">
+              <li className="flex items-center space-x-3 text-left sm:space-x-4">
                 <a
-                  class="flex h-6 w-6 items-center justify-center rounded-full bg-gray-600 text-xs font-semibold text-white ring ring-gray-600 ring-offset-2"
+                  className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-600 text-xs font-semibold text-white ring ring-gray-600 ring-offset-2"
                   href="#"
                 >
                   2
                 </a>
-                <span class="font-semibold text-gray-900">Shipping</span>
+                <span className="font-semibold text-gray-900">Shipping</span>
               </li>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-4 w-4 text-gray-400"
+                className="h-4 w-4 text-gray-400"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -79,93 +79,93 @@ function Transaction() {
                   d="M9 5l7 7-7 7"
                 />
               </svg>
-              <li class="flex items-center space-x-3 text-left sm:space-x-4">
+              <li className="flex items-center space-x-3 text-left sm:space-x-4">
                 <a
-                  class="flex h-6 w-6 items-center justify-center rounded-full bg-gray-400 text-xs font-semibold text-white"
+                  className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-400 text-xs font-semibold text-white"
                   href="#"
                 >
                   3
                 </a>
-                <span class="font-semibold text-gray-500">Payment</span>
+                <span className="font-semibold text-gray-500">Payment</span>
               </li>
             </ul>
           </div>
         </div>
       </div>
-      <div class="grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32">
-        <div class="px-4 pt-8">
-          <p class="text-xl font-medium">Order Summary</p>
-          <p class="text-gray-400">
+      <div className="grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32">
+        <div className="px-4 pt-8">
+          <p className="text-xl font-medium">Order Summary</p>
+          <p className="text-gray-400">
             Check your items. And select a suitable shipping method.
           </p>
-          <div class="mt-8 space-y-3 rounded-lg border bg-slate-600 px-2 py-4 sm:px-6 ">
+          <div className="mt-8 space-y-3 rounded-lg border bg-slate-600 px-2 py-4 sm:px-6 ">
             {cartItems.map((item) => (
-              <div class="flex flex-col rounded-lg bg-white sm:flex-row">
+              <div className="flex flex-col rounded-lg bg-white sm:flex-row">
                 <img
-                  class="m-2 h-24 w-28 rounded-md border object-cover object-center"
+                  className="m-2 h-24 w-28 rounded-md border object-cover object-center"
                   //   src={`http://localhost:8000/${product.image}`}
                   alt=""
                 />
-                <div class="flex w-full flex-col px-4 py-4">
-                  <span class="font-semibold">{item.name}</span>
-                  <span class="float-right text-gray-400">
+                <div className="flex w-full flex-col px-4 py-4">
+                  <span className="font-semibold">{item.name}</span>
+                  <span className="float-right text-gray-400">
                     Qty: {item.quantity}
                   </span>
-                  <p class="text-lg font-bold">Rp {item.price}</p>
+                  <p className="text-lg font-bold">Rp {item.price}</p>
                 </div>
               </div>
             ))}
           </div>
 
-          <p class="mt-8 text-lg font-medium">Shipping Methods</p>
-          <form class="mt-5 grid gap-6">
-            <div class="relative">
+          <p className="mt-8 text-lg font-medium">Shipping Methods</p>
+          <form className="mt-5 grid gap-6">
+            <div className="relative">
               <input
-                class="peer hidden"
+                className="peer hidden"
                 id="radio_1"
                 type="radio"
                 name="radio"
                 checked
               />
-              <span class="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
+              <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
               <label
-                class="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
+                className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
                 for="radio_1"
               >
                 <img
-                  class="w-14 object-contain"
+                  className="w-14 object-contain"
                   src="/images/naorrAeygcJzX0SyNI4Y0.png"
                   alt=""
                 />
-                <div class="ml-5">
-                  <span class="mt-2 font-semibold">Fedex Delivery</span>
-                  <p class="text-slate-500 text-sm leading-6">
+                <div className="ml-5">
+                  <span className="mt-2 font-semibold">Fedex Delivery</span>
+                  <p className="text-slate-500 text-sm leading-6">
                     Delivery: 2-4 Days
                   </p>
                 </div>
               </label>
             </div>
-            <div class="relative">
+            <div className="relative">
               <input
-                class="peer hidden"
+                className="peer hidden"
                 id="radio_2"
                 type="radio"
                 name="radio"
                 checked
               />
-              <span class="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
+              <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
               <label
-                class="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
+                className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
                 for="radio_2"
               >
                 <img
-                  class="w-14 object-contain"
+                  className="w-14 object-contain"
                   src="/images/oG8xsl3xsOkwkMsrLGKM4.png"
                   alt=""
                 />
-                <div class="ml-5">
-                  <span class="mt-2 font-semibold">Fedex Delivery</span>
-                  <p class="text-slate-500 text-sm leading-6">
+                <div className="ml-5">
+                  <span className="mt-2 font-semibold">Fedex Delivery</span>
+                  <p className="text-slate-500 text-sm leading-6">
                     Delivery: 2-4 Days
                   </p>
                 </div>
@@ -173,28 +173,28 @@ function Transaction() {
             </div>
           </form>
         </div>
-        <div class="mt-10 bg-gray-50 px-4 pt-8 lg:mt-0">
-          <p class="text-xl font-medium">Order Details</p>
-          <p class="text-gray-400">Check your order details.</p>
-          <div class="">
+        <div className="mt-10 bg-gray-50 px-4 pt-8 lg:mt-0">
+          <p className="text-xl font-medium">Order Details</p>
+          <p className="text-gray-400">Check your order details.</p>
+          <div className="">
             {/* <!-- Total --> */}
-            <div class="mt-6 border-t border-b py-2">
-              <div class="flex items-center justify-between">
-                <p class="text-sm font-medium text-gray-900">Subtotal</p>
-                <p class="font-semibold text-gray-900">Rp {subtotal}</p>
+            <div className="mt-6 border-t border-b py-2">
+              <div className="flex items-center justify-between">
+                <p className="text-sm font-medium text-gray-900">Subtotal</p>
+                <p className="font-semibold text-gray-900">Rp {subtotal}</p>
               </div>
-              <div class="flex items-center justify-between">
-                <p class="text-sm font-medium text-gray-900">Shipping</p>
-                <p class="font-semibold text-gray-900">Rp {shippingCost}</p>
+              <div className="flex items-center justify-between">
+                <p className="text-sm font-medium text-gray-900">Shipping</p>
+                <p className="font-semibold text-gray-900">Rp {shippingCost}</p>
               </div>
             </div>
-            <div class="mt-6 flex items-center justify-between">
-              <p class="text-sm font-medium text-gray-900">Total</p>
-              <p class="text-2xl font-semibold text-gray-900">Rp {total}</p>
+            <div className="mt-6 flex items-center justify-between">
+              <p className="text-sm font-medium text-gray-900">Total</p>
+              <p className="text-2xl font-semibold text-gray-900">Rp {total}</p>
             </div>
           </div>
           <button
-            class="mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white"
+            className="mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white"
             onClick={() => {
               navigate("/uploadPayment");
             }}
