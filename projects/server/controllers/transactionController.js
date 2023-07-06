8454af5bf2a9ed43059c0ebe42e97d0006123b9b
annisafrl <annisafirlia1@gmail.com>
2023-07-06 11:46:52 +0700

    fp-18 add sorting by order status

--- projects/server/controllers/transactionController.js
+++ projects/server/controllers/transactionController.js
@@ -4,6 +4,7 @@ module.exports = {
   fetchTransaction: async (req, res) => {
     try {
       const idUser = req.user.id;
+      const { status } = req.query;
 
       const transaction = await query(`
         SELECT *
@@ -21,6 +22,16 @@ module.exports = {
       `);
 
       res.status(200).send(transaction);
+    } catch (error) {
+      res.status(error.status || 500).send(error);
+    }
+  },
+  fetchTransactionStatus: async (req, res) => {
+    try {
+      const transactionStatus = await query(
+        `SELECT * FROM transactions_status`
+      );
+      return res.status(200).send(transactionStatus);
     } catch (error) {
       res.status(error.status || 500).send(error);
     }
