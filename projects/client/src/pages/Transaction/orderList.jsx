559bbaf696b047057d46a7f9ebf55df29d2764a4
annisafrl <annisafirlia1@gmail.com>
2023-07-07 18:00:52 +0700

    fp-18 add searchBar feature

--- projects/client/src/pages/Transaction/orderList.jsx
+++ projects/client/src/pages/Transaction/orderList.jsx
@@ -14,6 +14,7 @@ function OrderList() {
   const [currentPage, setCurrentPage] = useState(1);
   const [groupedTransactions, setGroupedTransactions] = useState({});
   const [filteredTransactions, setFilteredTransactions] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     fetchTransactions();
@@ -21,13 +22,17 @@ function OrderList() {
   }, []);
 
   useEffect(() => {
-    const filtered = Object.values(groupedTransactions).filter(
-      (group) =>
+    const filtered = Object.values(groupedTransactions).filter((group) => {
+      const transactionStatusMatch =
         selectedStatus === 0 ||
-        group.items[0].id_transaction_status === selectedStatus
-    );
+        group.items[0].id_transaction_status === selectedStatus;
+      const invoiceNumberMatch =
+        searchQuery === "" ||
+        group.items[0].invoiceNumber.includes(searchQuery);
+      return transactionStatusMatch && invoiceNumberMatch;
+    });
     setFilteredTransactions(filtered);
-  }, [selectedStatus, groupedTransactions]);
+  }, [selectedStatus, groupedTransactions, searchQuery]);
 
   useEffect(() => {
     const grouped = transactions.reduce((result, transaction) => {
@@ -100,6 +105,13 @@ function OrderList() {
     endIndex
   );
 
+  const handleSearch = (e) => {
+    const query = e.target.value;
+    setSearchQuery(query);
+
+    // Set ulang transaksi yang terfilter agar data diambil ulang
+  };
+
   return (
     <div>
       <div className="flex justify-center space-x-4 mt-8">
@@ -125,6 +137,44 @@ function OrderList() {
           </button>
         ))}
       </div>
+
+      <form className="flex items-center ml-52 mt-8">
+        <label htmlFor="simple-search" className="sr-only">
+          Search
+        </label>
+        <div className="relative w-1/4 justify-end">
+          <input
+            type="text"
+            id="simple-search"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search branch name..."
+            value={searchQuery}
+            onChange={handleSearch}
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          <svg
+            className="w-4 h-4"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+          <span className="sr-only">Search</span>
+        </button>
+      </form>
 
       {displayedTransactions.map((group) => (
         <div
