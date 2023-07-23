fda6d26b0b7e87197621430f14f37a65ff05018b
annisafrl <annisafirlia1@gmail.com>
2023-07-23 13:55:51 +0700

    Update orderList.jsx

--- projects/client/src/pages/Transaction/orderList.jsx
+++ projects/client/src/pages/Transaction/orderList.jsx
@@ -48,7 +48,7 @@ function OrderList() {
         );
       const invoiceNumberMatch =
         searchQuery === "" ||
-        group.items[0].invoiceNumber.toUpperCase().includes(searchQuery);
+        group.items[0]?.invoice_number?.toUpperCase().includes(searchQuery);
       return transactionStatusMatch && invoiceNumberMatch;
     });
     console.log("filter", filtered);
