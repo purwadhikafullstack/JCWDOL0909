08319bf7eb3de960401a2a91c26ac24a75b0da79
annisafrl <annisafirlia1@gmail.com>
2023-07-25 20:47:31 +0700

    edit responsive

--- projects/client/src/pages/Products/Product.jsx
+++ projects/client/src/pages/Products/Product.jsx
@@ -3,8 +3,8 @@ import ProductPage from "./ProductCard";
 
 const Products = () => {
   return (
-    <div className="grid grid-cols-1 gap-4 sm:w-full ">
-      <div className="col-start-2 col-span-6">
+    <div className="grid grid-cols-1 gap-4 sm:w-full mr-4">
+      <div className="col-start-1 col-span-6">
         <ProductPage />
       </div>
     </div>
